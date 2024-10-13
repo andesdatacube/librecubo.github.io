af8deeddbea5a7856b6597c1c396b894d692a8ac
ingrid1802 <ingridaybar841@gmail.com>
2024-10-13 13:54:27 -0500

    actualizacion

--- slider.js
+++
@@ -1,8 +0,0 @@
-const slider = document.getElementById("slider");
-const foregroundImg = document.querySelector(".foreground-img");
-const sliderButton = document.querySelector(".slider-button");
-
-slider.addEventListener("input", function () {
-  foregroundImg.style.width = `${this.value}%`;
-  sliderButton.style.left = `calc(${this.value}% - 15px)`;
-});
